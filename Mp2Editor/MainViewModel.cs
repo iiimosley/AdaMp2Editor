dd25d27e55c3dd60fde4681a9187f177e6affc71
Trey Mosley <iiimosley@gmail.com>
2024-09-29 22:41:41 -0500

    fix: update configs to compile targets correctly

--- Mp2Editor/MainViewModel.cs
+++ Mp2Editor/MainViewModel.cs
@@ -9,7 +9,6 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
-using LowProfile.Core.Ui;
 using Microsoft.Win32;
 using Mp2Editor.Core;
 
