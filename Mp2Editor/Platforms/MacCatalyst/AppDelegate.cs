c544e78e2992dd81d854be0b860cd16feaa98594
Trey Mosley <iiimosley@gmail.com>
2024-09-29 21:31:44 -0500

    chore: Add platforms

---
+++ Mp2Editor/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MP2Editor;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
